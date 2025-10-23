1d949e90721b470283baca51da30d2396ba4fe2c
novellis98 <umberto.novellis@gmail.com>
2025-10-23 19:52:06 +0200

    first commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,33 @@
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  output,
+  Output,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+  // imagePath = computed(() => 'assets/users/' + this.avatar());
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
+}
