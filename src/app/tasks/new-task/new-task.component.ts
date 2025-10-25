5c9c984201a4e62eb70f15fa63ba0a5cc565161e
novellis98 <umberto.novellis@gmail.com>
2025-10-26 00:25:16 +0200

    setted modules

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -6,14 +6,10 @@ import {
   Output,
   signal,
 } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { NewTaskData } from '../task/task.model';
 import { TasksService } from '../tasks.server';
 
 @Component({
   selector: 'app-new-task',
-  standalone: true,
-  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css',
 })
--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,12 +1,8 @@
 import { Component, inject, Input } from '@angular/core';
 import { type Task } from './task.model';
-import { CardComponent } from '../../shared/card/card.component';
-import { DatePipe } from '@angular/common';
 import { TasksService } from '../tasks.server';
 @Component({
   selector: 'app-task',
-  standalone: true,
-  imports: [CardComponent, DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
